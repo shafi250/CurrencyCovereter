ab74a3acb1444ef61558348fad121253f94d80b4
faisalhakim <malikfaisalhakim@gmail.com>
2020-05-14 15:59:37 +0430

    Third Commit

--- app/src/main/java/app/auaf/cconverter/ConvertCurrencyActivity.java
+++ app/src/main/java/app/auaf/cconverter/ConvertCurrencyActivity.java
@@ -7,8 +7,11 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +31,9 @@ public class ConvertCurrencyActivity extends AppCompatActivity {
     EditText etAmount;
     Button btnConvert;
     String date;
+    Spinner spinner_base_currency;
+    ImageView ivlogoCurrency;
+    String baseCurrenncy;
 
     TextView tvAfghani,tvInr,tvDerham;
     @Override
@@ -36,11 +42,12 @@ public class ConvertCurrencyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_convert_currency);
 
 
+        spinner_base_currency = (Spinner) findViewById(R.id.spinnerbase);
         etAmount=findViewById(R.id.etAmount);
         btnConvert=findViewById(R.id.btnConvert);
         tvAfghani=findViewById(R.id.tvAfghaniValue);
-        tvInr=findViewById(R.id.tvINRValue);
-        tvDerham=findViewById(R.id.tvEmirateDerhamVlaue);
+
+        ivlogoCurrency=findViewById(R.id.ivlogoCurrency);
 
 
 
@@ -48,23 +55,72 @@ public class ConvertCurrencyActivity extends AppCompatActivity {
 
 
 
-        etAmount.addTextChangedListener(new TextWatcher() {
+        spinner_base_currency.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Log.d("Convert",""+position);
+
+
+                switch (position){
+
+                    case 0:
+                        ivlogoCurrency.setImageResource(R.drawable.ic_afg);
+                        baseCurrenncy="AFN";
+                        break;
+                    case 1:
+                        ivlogoCurrency.setImageResource(R.drawable.ic_usa);
+                        baseCurrenncy="USD";
+                        break;
+
+                    case 2:
+                        ivlogoCurrency.setImageResource(R.drawable.ic_india);
+                        baseCurrenncy="INR";
+                        break;
+
+                    case 3:
+                        ivlogoCurrency.setImageResource(R.drawable.ic_euro);
+                        baseCurrenncy="EUR";
+                        break;
+                }
 
             }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-            }
 
             @Override
-            public void afterTextChanged(Editable s) {
+            public void onNothingSelected(AdapterView<?> parent) {
 
-                getValueOnlineFromAPI();
             }
         });
+
+
+
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+getValueOnlineFromAPI();
+            }
+        });
+
+
+//        etAmount.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//                getValueOnlineFromAPI();
+//            }
+//        });
 
 
 
@@ -74,45 +130,64 @@ public class ConvertCurrencyActivity extends AppCompatActivity {
     private void getValueOnlineFromAPI() {
 
 
-            String url = "http://data.fixer.io/api/latest\n" +
-                    "?access_key=501735be2fd52744492f54d468993c4a";
+            String url = "https://free.currconv.com/api/v7/convert?q="+baseCurrenncy+"_AFN&compact=ultra&apiKey=cf56bbee1671257934e4";
 
-            JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+
+            JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
                     url, null, new Response.Listener<JSONObject>() {
 
                 @Override
                 public void onResponse(JSONObject response) {
                     Log.d("Tag", response.toString());
-                    try {
-                        // Parsing json object response
-                        // response will be a json object
-
-                        date = response.getString("date");
-                        JSONObject data = response.getJSONObject("rates");
-
-                        String AFN = data.getString("AFN");
-                        String INR = data.getString("INR");
-                        String AED = data.getString("AED");
-
-                        double afnRate = Double.parseDouble(AFN);
-                        double inrRate = Double.parseDouble(INR);
-                        double aedRate= Double.parseDouble(AED);
 
 
-                        double amountConvert = Double.parseDouble(etAmount.getText().toString());
+                                           try {
+                                               String valueConverted = response.getString(""+baseCurrenncy+"_AFN");
+
+                                               double valueAmount=Double.parseDouble(etAmount.getText().toString());
+                                               double valueConverteddouble=Double.parseDouble(valueConverted);
 
 
-                        tvAfghani.setText(amountConvert* afnRate+" Afghani");
-                        tvDerham.setText(amountConvert* aedRate+" Derham");
-                        tvInr.setText(amountConvert*inrRate+" Rupees");
 
 
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        Toast.makeText(getApplicationContext(),
-                                "Error: " + e.getMessage(),
-                                Toast.LENGTH_LONG).show();
-                    }
+                                               Log.d("Tag", "value: "+valueConverted);
+                                               tvAfghani.setText(valueAmount*valueConverteddouble+" Afghani");
+                                           }catch (JSONException e){
+
+                                               Log.d("Tag", "onResponse: "+e);
+
+                                           }
+                 //       JSONObject data = response.getJSONObject("rates");
+//                    try {
+//                        // Parsing json object response
+//                        // response will be a json object
+//
+//                        date = response.getString("date");
+//                        JSONObject data = response.getJSONObject("rates");
+//
+//                        String AFN = data.getString("AFN");
+//                        String INR = data.getString("INR");
+//                        String AED = data.getString("AED");
+//
+//                        double afnRate = Double.parseDouble(AFN);
+//                        double inrRate = Double.parseDouble(INR);
+//                        double aedRate= Double.parseDouble(AED);
+//
+//
+//                        double amountConvert = Double.parseDouble(etAmount.getText().toString());
+//
+//
+//                        tvAfghani.setText(amountConvert* afnRate+" Afghani");
+//                        tvDerham.setText(amountConvert* aedRate+" Derham");
+//                        tvInr.setText(amountConvert*inrRate+" Rupees");
+//
+//
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                        Toast.makeText(getApplicationContext(),
+//                                "Error: " + e.getMessage(),
+//                                Toast.LENGTH_LONG).show();
+//                    }
 
 
                 }
